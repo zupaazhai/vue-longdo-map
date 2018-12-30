efc99af748f08116d960c987629de801dfad67f8
supachai <supachai@outlook.com>
2018-12-30 15:28:03 +0700

    build 0.3.0

--- src/components.js
+++ src/components.js
@@ -1,5 +1,9 @@
-import Banner from './components/Banner.vue'
+import LongdoMap from './components/LongdoMap.vue'
+import LongdoMapMarker from './components/LongdoMapMarker.vue'
+import LongdoMapPopup from './components/LongdoMapPopup.vue'
 
 export {
-  Banner
+  LongdoMap,
+  LongdoMapMarker,
+  LongdoMapPopup
 }
